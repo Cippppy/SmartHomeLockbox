ecaab9653ee591089cde5d4b043a980bbef7f1cb
Christian Cipolletta <98924381+Cippppy@users.noreply.github.com>
2023-05-05 13:13:06 -0400

    Update I2C.c

--- Drivers/I2C.c
+++ Drivers/I2C.c
@@ -1,24 +1,16 @@
+/**
+ * I2C.c
+ *  This file is based off the TI Resource named, "msp430fr235x_eusci_i2c_standard_master.c"
+ *  It uses the functions inside the resource to try and make i2c work for this project.
+ *      author: Christian Cipolletta
+ *      version: 5/5/2023
+ */
+
 #include <msp430.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include "I2C.h"
 #include "Adafruit_BME280.h"
-
-
-/* MasterTypeX are example buffers initialized in the master, they will be
- * sent by the master to the slave.
- * SlaveTypeX are example buffers initialized in the slave, they will be
- * sent by the slave to the master.
- * */
-
-uint8_t MasterType2 [TYPE_2_LENGTH] = {'F', '4', '1', '9', '2', 'B'};
-uint8_t MasterType1 [TYPE_1_LENGTH] = { 8, 9};
-uint8_t MasterType0 [TYPE_0_LENGTH] = { 11};
-
-
-uint8_t SlaveType2 [TYPE_2_LENGTH] = {0};
-uint8_t SlaveType1 [TYPE_1_LENGTH] = {0};
-uint8_t SlaveType0 [TYPE_0_LENGTH] = {0};
 
 /* Used to track the state of the software state machine*/
 I2C_Mode MasterMode = IDLE_MODE;
@@ -40,6 +32,17 @@ uint8_t TransmitBuffer[MAX_BUFFER_SIZE] = {0};
 uint8_t TXByteCtr = 0;
 uint8_t TransmitIndex = 0;
 
+/* For slave device with dev_addr, writes the data specified in *reg_data
+ *
+ * dev_addr: The slave device address.
+ *           Example: SLAVE_ADDR
+ * reg_addr: The register or command to send to the slave.
+ *           Example: CMD_TYPE_0_MASTER
+ * *reg_data: The buffer to write
+ *           Example: MasterType0
+ * count: The length of *reg_data
+ *           Example: TYPE_0_LENGTH
+ *  */
 I2C_Mode I2CB1_Master_ReadReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t count)
 {
     /* Initialize state machine */
@@ -63,7 +66,16 @@ I2C_Mode I2CB1_Master_ReadReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t count)
 
 }
 
-
+/* For slave device with dev_addr, read the data specified in slaves reg_addr.
+ * The received data is available in ReceiveBuffer
+ *
+ * dev_addr: The slave device address.
+ *           Example: SLAVE_ADDR
+ * reg_addr: The register or command to send to the slave.
+ *           Example: CMD_TYPE_0_SLAVE
+ * count: The length of data to read
+ *           Example: TYPE_0_LENGTH
+ *  */
 I2C_Mode I2CB1_Master_WriteReg(uint8_t dev_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t count)
 {
     /* Initialize state machine */
@@ -97,119 +109,4 @@ void CopyArray(uint8_t *source, uint8_t *dest, uint8_t count)
     {
         dest[copyIndex] = source[copyIndex];
     }
-}
-
-float readTemperature(void)
-{
-
-    float temperature = 0;
-
-    int32_t var1, var2;
-
-    int32_t adc_T = I2CB1_Master_ReadReg(BME280_ADDRESS, BME280_REGISTER_TEMPDATA, 8);
-
-    adc_T >>= 4;
-
-    var1  = ((((adc_T>>3) - ((int32_t)cal_data.dig_T1 <<1))) *
-
-             ((int32_t)cal_data.dig_T2)) >> 11;
-
-    var2  = (((((adc_T>>4) - ((int32_t)cal_data.dig_T1)) *
-
-               ((adc_T>>4) - ((int32_t)cal_data.dig_T1))) >> 12) *
-
-             ((int32_t)cal_data.dig_T3)) >> 14;
-
-    t_fine = var1 + var2;
-
-
-    temperature  = (t_fine * 5 + 128) >> 8;
-
-    temperature = temperature / 100;
-
-    return temperature;
-}
-
-float readPressure(void) {
-
-    float pressure = 0;
-
-    int64_t var1, var2, p;
-
-    int32_t adc_P = I2CB1_Master_ReadReg(BME280_ADDRESS, BME280_REGISTER_PRESSUREDATA, 24);
-    adc_P >>= 4;
-
-    var1 = ((int64_t)t_fine) - 128000;
-
-    var2 = var1 * var1 * (int64_t)cal_data.dig_P6;
-
-    var2 = var2 + ((var1*(int64_t)cal_data.dig_P5)<<17);
-
-    var2 = var2 + (((int64_t)cal_data.dig_P4)<<35);
-
-    var1 = ((var1 * var1 * (int64_t)cal_data.dig_P3)>>8) +
-
-    ((var1 * (int64_t)cal_data.dig_P2)<<12);
-
-    var1 = (((((int64_t)1)<<47)+var1))*((int64_t)cal_data.dig_P1)>>33;
-
-
-    if (var1 == 0) {
-
-        // return 0;  // avoid exception caused by division by zero
-        pressure = 0.0;
-    }
-
-    p = 1048576 - adc_P;
-
-    p = (((p<<31) - var2)*3125) / var1;
-
-    var1 = (((int64_t)cal_data.dig_P9) * (p>>13) * (p>>13)) >> 25;
-
-    var2 = (((int64_t)cal_data.dig_P8) * p) >> 19;
-
-
-    p = ((p + var1 + var2) >> 8) + (((int64_t)cal_data.dig_P7)<<4);
-
-    pressure = (float)p/256;
-
-    return pressure;
-}
-
-float readHumidity(void) {
-
-    float humidity = 0;
-
-    int32_t adc_H = I2CB1_Master_ReadReg(BME280_ADDRESS, BME280_REGISTER_HUMIDDATA, 16);
-
-    int32_t v_x1_u32r;
-
-    v_x1_u32r = (t_fine - ((int32_t)76800));
-
-    v_x1_u32r = (((((adc_H << 14) - (((int32_t)cal_data.dig_H4) << 20) -
-
-                    (((int32_t)cal_data.dig_H5) * v_x1_u32r)) + ((int32_t)16384)) >> 15) *
-
-                 (((((((v_x1_u32r * ((int32_t)cal_data.dig_H6)) >> 10) *
-
-                      (((v_x1_u32r * ((int32_t)cal_data.dig_H3)) >> 11) + ((int32_t)32768))) >> 10) +
-
-                    ((int32_t)2097152)) * ((int32_t)cal_data.dig_H2) + 8192) >> 14));
-
-
-    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >> 7) *
-
-                               ((int32_t)cal_data.dig_H1)) >> 4));
-
-
-    v_x1_u32r = (v_x1_u32r < 0) ? 0 : v_x1_u32r;
-
-    v_x1_u32r = (v_x1_u32r > 419430400) ? 419430400 : v_x1_u32r;
-
-    float h = (v_x1_u32r>>12);
-
-    // return  h / 1024.0;
-    humidity = h / 1024.0;
-
-    return humidity;
 }
